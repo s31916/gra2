54088bceb49e35fc52342d8e089d218c6d770e3a
s31916 <s31916@pjwstk.edu.pl>
2025-05-26 15:22:44 +0200

    e
    gowno

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public float moveSpeed = 5;
     public float runSpeed = 20;
-    public float jumpforce = 3000;
+    public float jumpforce = 300;
+
+    public GroundChecker groundChecker;
     void Start()
     {
       rb = GetComponent<Rigidbody2D>();
@@ -29,10 +31,11 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && groundChecker.isGrounded)
         {
             //rb.AddForce(new Vector2(0, jumpforce));
             rb.AddForce(Vector2.up * jumpforce);
         }
     }
+
 }
